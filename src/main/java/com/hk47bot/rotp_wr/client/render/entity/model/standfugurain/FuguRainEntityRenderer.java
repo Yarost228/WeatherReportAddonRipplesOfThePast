a558aaede7b9124d1d0f5c03af3a01a820606add
Yarost228 <121579753+Yarost228@users.noreply.github.com>
2023-09-03 19:01:41 +0300

    added FuguRainEntityRenderer.java

--- src/main/java/com/hk47bot/rotp_wr/client/render/entity/model/standfugurain/FuguRainEntityRenderer.java
+++ src/main/java/com/hk47bot/rotp_wr/client/render/entity/model/standfugurain/FuguRainEntityRenderer.java
@@ -1,1 +1,19 @@
+package com.hk47bot.rotp_wr.client.render.entity.model.standfugurain;
 
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.inventory.container.PlayerContainer;
+import net.minecraft.util.ResourceLocation;
+
+public class FuguRainEntityRenderer extends EntityRenderer<Entity> {
+
+	public FuguRainEntityRenderer(EntityRendererManager renderManager) {
+		super(renderManager);
+	}
+
+	@Override
+	public ResourceLocation getTextureLocation(Entity entity) {
+		return PlayerContainer.BLOCK_ATLAS;
+	}
+}
