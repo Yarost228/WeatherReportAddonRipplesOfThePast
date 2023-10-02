a4353df0db8c15ad5db6b390eb3d60276c67d979
Yarost228 <121579753+Yarost228@users.noreply.github.com>
2023-10-02 21:13:36 +0300

    Update WeatherReportChangeWeather.java

--- src/main/java/com/hk47bot/rotp_wr/action/stand/WeatherReportChangeWeather.java
+++ src/main/java/com/hk47bot/rotp_wr/action/stand/WeatherReportChangeWeather.java
@@ -6,28 +6,29 @@ import com.github.standobyte.jojo.entity.stand.StandEntityTask;
 import com.github.standobyte.jojo.entity.stand.StandPose;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.LogicalSide;
-import net.minecraftforge.fml.LogicalSidedProvider;
 
 public class WeatherReportChangeWeather extends StandEntityAction {
     public static final StandPose WEATHER_CHANGE_POSE = new StandPose("WR_WEATHERCHANGE");
 
+
     public WeatherReportChangeWeather(StandEntityAction.Builder builder) {
         super(builder);
 }
+    
    @Override
     
     public void standPerform(World world, StandEntity standEntity, IStandPower userPower, StandEntityTask task) {
         if (!world.isClientSide()) {
-            MinecraftServer source = LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER);
-            if (!world.isRaining()) {
-            source.getCommands().performCommand(source.createCommandSourceStack(), "weather rain");
+            if (!world.isRaining()) {;
+            world.getLevelData().setRaining(true);
             }
-            else {
-                source.getCommands().performCommand(source.createCommandSourceStack(), "weather clear");
+            else {;
+            world.getLevelData().setRaining(false);
             }
         }
     }
+    
+  
+
 }
