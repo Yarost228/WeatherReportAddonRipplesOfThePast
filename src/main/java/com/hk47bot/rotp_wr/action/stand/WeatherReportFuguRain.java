f1fdfe82ae3fbea0d536dedd446ad3e73410d255
Yarost228 <121579753+Yarost228@users.noreply.github.com>
2023-10-02 23:03:14 +0300

    Update WeatherReportFuguRain.java

--- src/main/java/com/hk47bot/rotp_wr/action/stand/WeatherReportFuguRain.java
+++ src/main/java/com/hk47bot/rotp_wr/action/stand/WeatherReportFuguRain.java
@@ -1,11 +1,14 @@
 package com.hk47bot.rotp_wr.action.stand;
 
+import com.github.standobyte.jojo.action.ActionConditionResult;
+import com.github.standobyte.jojo.action.ActionTarget;
 import com.github.standobyte.jojo.action.stand.StandEntityAction;
 import com.github.standobyte.jojo.entity.stand.StandEntity;
 import com.github.standobyte.jojo.entity.stand.StandEntityTask;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import com.hk47bot.rotp_wr.entity.fugurain.FugaSummonerEntity;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
@@ -17,6 +20,20 @@ public class WeatherReportFuguRain extends StandEntityAction {
         super(builder);
     }
     
+    @Override
+    protected ActionConditionResult checkSpecificConditions(LivingEntity user, IStandPower power, ActionTarget target) {
+        BlockPos userPos = new BlockPos(user.getPosition(1));
+        if (!user.level.canSeeSky(userPos)){
+                    return conditionMessage("fugurain-warningneedtoseesky");
+                }
+        if (!user.level.getLevelData().isRaining()){
+                    return conditionMessage("fugurain-warningrain");
+                }
+        else {
+            return super.checkSpecificConditions(user, power, target);
+        }
+
+    }
 
     @Override
     public void standPerform(World world, StandEntity standEntity, IStandPower userPower, StandEntityTask task) {
@@ -34,11 +51,7 @@ public class WeatherReportFuguRain extends StandEntityAction {
                 world.addFreshEntity(summoner);
 
                 }
-                else if (!world.getLevelData().isRaining()){
-                    conditionMessage("fugurain-warningrain");
-                }
             }
         }
     }
 }
-
