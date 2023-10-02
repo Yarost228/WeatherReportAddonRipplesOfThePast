a970812650f5db6f65aeeb5adfbc21a0123e8fef
Yarost228 <121579753+Yarost228@users.noreply.github.com>
2023-10-02 21:05:52 +0300

    Update InitEntities.java

--- src/main/java/com/hk47bot/rotp_wr/init/InitEntities.java
+++ src/main/java/com/hk47bot/rotp_wr/init/InitEntities.java
@@ -1,11 +1,20 @@
 package com.hk47bot.rotp_wr.init;
 
 import com.hk47bot.rotp_wr.RotpWeatherReportAddon;
+import com.hk47bot.rotp_wr.entity.fugurain.FugaSummonerEntity;
 
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class InitEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, RotpWeatherReportAddon.MOD_ID);
+    
+    public static final RegistryObject<EntityType<FugaSummonerEntity>> FUGU_SUMMONER = ENTITIES.register("fuga_summoner",
+     () -> EntityType.Builder.<FugaSummonerEntity>of(FugaSummonerEntity::new, EntityClassification.MISC)
+    .fireImmune()
+    .sized(0.25F, 0.25F)
+    .build(RotpWeatherReportAddon.MOD_ID + ":fuga_summoner"));
 };
