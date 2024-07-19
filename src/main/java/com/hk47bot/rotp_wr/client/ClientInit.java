d42dcfd2af528af264078c36fbc0b54297f0d6f4
Yarost228 <yarik.alferov1@gmail.com>
2024-07-19 10:17:54 +0300

    Update ClientInit.java

--- src/main/java/com/hk47bot/rotp_wr/client/ClientInit.java
+++ src/main/java/com/hk47bot/rotp_wr/client/ClientInit.java
@@ -1,12 +1,17 @@
 package com.hk47bot.rotp_wr.client;
 
+import com.github.standobyte.jojo.init.ModBlocks;
 import com.hk47bot.rotp_wr.RotpWeatherReportAddon;
 import com.hk47bot.rotp_wr.client.render.entity.renderer.FugaProjectileRenderer;
 import com.hk47bot.rotp_wr.client.render.entity.renderer.FugaSummonerEntityRenderer;
+import com.hk47bot.rotp_wr.client.render.entity.renderer.IcicleExtendingProjectileRenderer;
 import com.hk47bot.rotp_wr.client.render.entity.renderer.stand.WeatherReportRenderer;
 import com.hk47bot.rotp_wr.init.AddonStands;
+import com.hk47bot.rotp_wr.init.InitBlocks;
 import com.hk47bot.rotp_wr.init.InitEntities;
 
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -21,5 +26,9 @@ public class ClientInit {
         RenderingRegistry.registerEntityRenderingHandler(AddonStands.WEATHER_REPORT.getEntityType(), WeatherReportRenderer::new);
         RenderingRegistry.registerEntityRenderingHandler(InitEntities.FUGU_SUMMONER.get(), FugaSummonerEntityRenderer::new);
         RenderingRegistry.registerEntityRenderingHandler(InitEntities.FUGU_PROJECTILE.get(), FugaProjectileRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(InitEntities.ICICLE_STRIKE_PROJECTILE.get(), IcicleExtendingProjectileRenderer::new);
+
+        RenderTypeLookup.setRenderLayer(InitBlocks.BLOOD_PUDDLE.get(), RenderType.cutoutMipped());
+        RenderTypeLookup.setRenderLayer(InitBlocks.BLOOD_SPIKES.get(), RenderType.cutoutMipped());
     }
 }
