64ba28325e09213384acc453059386449b37dc88
Yarost228 <yarik.alferov1@gmail.com>
2025-01-12 01:17:46 +0300

    Removed redundant code

--- src/main/java/com/hk47bot/rotp_wr/client/render/entity/renderer/stand/WeatherReportRenderer.java
+++ src/main/java/com/hk47bot/rotp_wr/client/render/entity/renderer/stand/WeatherReportRenderer.java
@@ -5,8 +5,6 @@ import com.hk47bot.rotp_wr.client.render.entity.model.stand.WeatherReportModel;
 import com.hk47bot.rotp_wr.entity.stand.stands.WeatherReportEntity;
 import com.github.standobyte.jojo.client.render.entity.renderer.stand.StandEntityRenderer;
 
-import com.hk47bot.rotp_wr.init.InitStands;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 
@@ -14,16 +12,5 @@ public class WeatherReportRenderer extends StandEntityRenderer<WeatherReportEnti
 
     public WeatherReportRenderer(EntityRendererManager renderManager) {
         super(renderManager, new WeatherReportModel(), new ResourceLocation(RotpWeatherReportAddon.MOD_ID, "textures/entity/stand/weather_report.png"), 0);
-    }
-    @Override
-    protected ViewObstructionPrevention obstructsView(WeatherReportEntity entity, float partialTick) {
-        super.obstructsView(entity, partialTick);
-        Minecraft mc = Minecraft.getInstance();
-        if (mc.options.getCameraType().isFirstPerson() && entity.isCloseToUser()) {
-            if (entity.getCurrentTaskAction() == InitStands.WEATHER_REPORT_BLOCK.get() || entity.getCurrentTaskAction() == InitStands.WEATHER_REPORT_ICICLE_STRIKE.get()){
-                return ViewObstructionPrevention.ARMS_ONLY;
-            }
-        }
-        return ViewObstructionPrevention.NONE;
     }
 }
