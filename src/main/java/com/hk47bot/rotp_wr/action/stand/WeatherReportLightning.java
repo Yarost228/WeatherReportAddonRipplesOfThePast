4e3b3916c1d8cac320a1d84495fb31f3334143ca
Yarost228 <yarik.alferov1@gmail.com>
2025-01-12 01:18:27 +0300

    Added particle to pos of strike

--- src/main/java/com/hk47bot/rotp_wr/action/stand/WeatherReportLightning.java
+++ src/main/java/com/hk47bot/rotp_wr/action/stand/WeatherReportLightning.java
@@ -7,11 +7,10 @@ import com.github.standobyte.jojo.entity.stand.StandEntity;
 import com.github.standobyte.jojo.entity.stand.StandEntityTask;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 
-import com.github.standobyte.jojo.util.general.GeneralUtil;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.LightningBoltEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
@@ -22,32 +21,39 @@ public class WeatherReportLightning extends StandEntityAction {
         super(builder);
     }
 
+    @Override
+    public void onHoldTick(World world, LivingEntity user,IStandPower power, int ticksHeld, ActionTarget target, boolean requirementsFulfilled) {
+        super.holdTick(world, user, power, ticksHeld, target, requirementsFulfilled);
+        if (requirementsFulfilled && world.isClientSide()) {
+            StandEntity standEntity = (StandEntity) power.getStandManifestation();
+            RayTraceResult result = standEntity.precisionRayTrace(standEntity, 100);
+            Vector3d pos = result.getLocation();
+            world.addParticle(ParticleTypes.END_ROD, pos.x, pos.y(), pos.z(), 0, 0, 0);
+        }
+    }
+
 
     @Override
     public void standPerform(World world, StandEntity standEntity, IStandPower userPower, StandEntityTask task) {
         if (!world.isClientSide()) {
-            RayTraceResult target = standEntity.aimWithStandOrUser(100, task.getTarget());
-            Vector3d pos = target.getLocation();
+            RayTraceResult result = standEntity.precisionRayTrace(standEntity, 100);
+            Vector3d pos = result.getLocation();
             if (pos != null) {
-                int count;
-                if (world.isRaining()){
-                    if (world.isThundering()){
-                        count = 9;
-                    }
-                    else {
-                        count = 6;
-                    }
+                int lightningCount;
+                if (world.isThundering()){
+                    lightningCount = 8;
+                }
+                else if (!world.isRaining()){
+                    lightningCount = 0;
+
                 }
                 else {
-                    count = 3;
+                    lightningCount = 3;
                 }
-                GeneralUtil.doFractionTimes(() -> {
-                    LightningBoltEntity bolt = EntityType.LIGHTNING_BOLT.create(world);
-                    bolt.moveTo(pos);
-                    bolt.setDamage(7);
-                    bolt.setCause((ServerPlayerEntity) userPower.getUser());
-                    world.addFreshEntity(bolt);
-                }, count);
+                LightningBoltEntity bolt = EntityType.LIGHTNING_BOLT.create(world);
+                bolt.moveTo(pos);
+                bolt.setDamage(7 + lightningCount);
+                world.addFreshEntity(bolt);
             }
         }
     }
